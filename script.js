2563b7bc7b6432db2ae4226c406b8fc360d9edaa
Geesith <kgpgkariyawasam@students.nsbm.ac.lk>
2025-12-07 22:59:48 +0530

    activate category slider

--- script.js
+++ script.js
@@ -41,3 +41,33 @@
     cloneSlides();
     updateSlider();
     setInterval(nextSlide, 10000);
+
+
+    // type slider begins
+    const sha = document.getElementById('typeTrack');
+    let types = Array.from(document.querySelectorAll('.type'));
+    const totalTypes = types.length / 2; // original types count
+    let index = 0;
+
+    // Move one slide
+    function moveSlide() {
+      index++;
+      sha.style.transition = 'transform 0.5s ease-in-out';
+      sha.style.transform = `translateX(-${index * (100 / totalTypes)}%)`;
+
+      // Reset for seamless infinite loop
+      if (index >= totalTypes) {
+        setTimeout(() => {
+          sha.style.transition = 'none';
+          index = 0;
+          sha.style.transform = `translateX(0)`;
+          void sha.offsetWidth; // force reflow
+          sha.style.transition = 'transform 5s ease-in-out';
+        }, 500);
+      }
+    }
+
+    // Move one slide every 4 seconds
+    setInterval(moveSlide, 4000);
+
+    // type slider ends
